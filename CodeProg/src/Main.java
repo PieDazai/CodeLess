068acf82cdf043e5aa2be48ad5e3755a55876b7b
1 <kir.it.prog@gmail.com>
2024-09-14 17:56:48 +0700

    swith

--- CodeProg/src/Main.java
+++ CodeProg/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("выберите товар!\n1)шоколад\n2)кофе\n3)чай");
+        Scanner ss = new Scanner(System.in);
+        int num = ss.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("шоколад");
+                System.out.println("вкусный");
+                break;
+            case 2:
+                System.out.println("кофе");
+                break;
+            case 3:
+                System.out.println("чай");
+                break;
+        }
     }
 }
